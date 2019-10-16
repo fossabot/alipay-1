c21a7b9d6a133305563367ef88febfade03b7e7d
kaylv <kaylv@dayuw.com>
2019-10-16 17:58:50 +0800

    完善剩余当面付接口:取消,关闭,退款

--- src/F2fpay/Order/Client.php
+++ src/F2fpay/Order/Client.php
@@ -2,9 +2,14 @@
 
 namespace Kaylyu\Alipay\F2fpay\Order;
 
+use Kaylyu\Alipay\F2fpay\Base\Aop\Request\AlipayTradeCancelRequest;
+use Kaylyu\Alipay\F2fpay\Base\Aop\Request\AlipayTradeCloseRequest;
+use Kaylyu\Alipay\F2fpay\Base\Aop\Request\AlipayTradeCreateRequest;
 use Kaylyu\Alipay\F2fpay\Base\Aop\Request\AlipayTradePayRequest;
 use Kaylyu\Alipay\F2fpay\Base\Aop\Request\AlipayTradePrecreateRequest;
 use Kaylyu\Alipay\F2fpay\Base\Aop\Request\AlipayTradeQueryRequest;
+use Kaylyu\Alipay\F2fpay\Base\Model\Builder\AlipayTradeCancelContentBuilder;
+use Kaylyu\Alipay\F2fpay\Base\Model\Builder\AlipayTradeCloseContentBuilder;
 use Kaylyu\Alipay\F2fpay\Base\Model\Builder\AlipayTradePayContentBuilder;
 use Kaylyu\Alipay\F2fpay\Base\Model\Builder\AlipayTradePrecreateContentBuilder;
 use Kaylyu\Alipay\F2fpay\Base\Model\Builder\AlipayTradeQueryContentBuilder;
@@ -23,7 +28,9 @@ class Client extends BaseClient
 {
     /**
      * 统一收单交易支付接口（条码支付）
+     *
      * 收银员使用扫码设备读取用户手机支付宝“付款码”/声波获取设备（如麦克风）读取用户手机支付宝的声波信息后，将二维码或条码信息/声波信息通过本接口上送至支付宝发起支付
+     *
      * @param AlipayTradePayContentBuilder $builder
      * @author kaylv <kaylv@dayuw.com>
      * @return array|\Kaylyu\Alipay\Kernel\Support\Collection|string
@@ -58,7 +65,9 @@ class Client extends BaseClient
 
     /**
      * 统一收单线下交易预创建（扫码支付）
+     *
      * 收银员通过收银台或商户后台调用支付宝接口，生成二维码后，展示给用户，由用户扫描二维码完成订单支付
+     *
      * @param AlipayTradePrecreateContentBuilder $builder
      * @author kaylv <kaylv@dayuw.com>
      * @return array|\Kaylyu\Alipay\Kernel\Support\Collection|string
@@ -97,6 +106,13 @@ class Client extends BaseClient
 
     /**
      * 统一收单线下交易查询
+     *
+     * 该接口提供所有支付宝支付订单的查询，商户可以通过该接口主动查询订单状态，完成下一步的业务逻辑。 需要调用查询接口的情况：
+     * 当商户后台、网络、服务器等出现异常，商户系统最终未接收到支付通知；
+     * 调用支付接口后，返回系统错误或未知交易状态情况；
+     * 调用alipay.trade.pay，返回INPROCESS的状态；
+     * 调用alipay.trade.cancel之前，需确认支付状态
+     *
      * @param AlipayTradeQueryContentBuilder $builder
      * @author kaylv <kaylv@dayuw.com>
      * @return array|\Kaylyu\Alipay\Kernel\Support\Collection|string
@@ -119,6 +135,86 @@ class Client extends BaseClient
 
         //处理
         if (querySuccess($data)) {
+            // 查询返回该订单交易支付成功
+            $result->setTradeStatus(AlipayF2FPayResult::ALIPAY_F2FPAY_RESULT_SUCCESS);
+        } elseif (tradeError($data)) {
+            //查询发生异常或无返回，交易状态未知
+            $result->setTradeStatus(AlipayF2FPayResult::ALIPAY_F2FPAY_RESULT_UNKNOWN);
+        } else {
+            //其他情况均表明该订单号交易失败
+            $result->setTradeStatus(AlipayF2FPayResult::ALIPAY_F2FPAY_RESULT_FAILED);
+        }
+
+        return $this->formatResponseToType($result, $this->app->config->get('response_type'));
+    }
+
+    /**
+     * 统一收单交易撤销接口
+     *
+     * 支付交易返回失败或支付系统超时，调用该接口撤销交易。如果此订单用户支付失败，支付宝系统会将此订单关闭；如果用户支付成功，支付宝系统会将此订单资金退还给用户
+     * 注意：只有发生支付系统超时或者支付结果未知时可调用撤销，其他正常支付的单如需实现相同功能请调用申请退款API
+     * 提交支付交易后调用【查询订单API】，没有明确的支付结果再调用【撤销订单API】
+     *
+     * @param AlipayTradeCancelContentBuilder $builder
+     * @author kaylv <kaylv@dayuw.com>
+     * @return array|\Kaylyu\Alipay\Kernel\Support\Collection|string
+     */
+    public function cancel(AlipayTradeCancelContentBuilder $builder)
+    {
+        $request = new AlipayTradeCancelRequest();
+        $request->setBizContent($builder->getBizContent());
+
+        //请求
+        $response = $this->httpPost($request, $builder->getAppAuthToken());
+
+        //获取
+        $data = $response->alipay_trade_cancel_response;
+        $sign = $response->sign;
+
+        //组装返回数据
+        $result = new AlipayF2FPayResult($data, $sign);
+
+        //处理
+        if (tradeSuccess($data)) {
+            // 查询返回该订单交易支付成功
+            $result->setTradeStatus(AlipayF2FPayResult::ALIPAY_F2FPAY_RESULT_SUCCESS);
+        } elseif (tradeError($data)) {
+            //查询发生异常或无返回，交易状态未知
+            $result->setTradeStatus(AlipayF2FPayResult::ALIPAY_F2FPAY_RESULT_UNKNOWN);
+        } else {
+            //其他情况均表明该订单号交易失败
+            $result->setTradeStatus(AlipayF2FPayResult::ALIPAY_F2FPAY_RESULT_FAILED);
+        }
+
+        return $this->formatResponseToType($result, $this->app->config->get('response_type'));
+    }
+
+    /**
+     * 统一收单交易关闭接口
+     *
+     * 用于交易创建后，用户在一定时间内未进行支付，可调用该接口直接将未付款的交易进行关闭
+     *
+     * @param AlipayTradeCloseContentBuilder $builder
+     * @author kaylv <kaylv@dayuw.com>
+     * @return array|\Kaylyu\Alipay\Kernel\Support\Collection|string
+     */
+    public function close(AlipayTradeCloseContentBuilder $builder)
+    {
+        $request = new AlipayTradeCloseRequest();
+        $request->setBizContent($builder->getBizContent());
+
+        //请求
+        $response = $this->httpPost($request, $builder->getAppAuthToken());
+
+        //获取
+        $data = $response->alipay_trade_close_response;
+        $sign = $response->sign;
+
+        //组装返回数据
+        $result = new AlipayF2FPayResult($data, $sign);
+
+        //处理
+        if (tradeSuccess($data)) {
             // 查询返回该订单交易支付成功
             $result->setTradeStatus(AlipayF2FPayResult::ALIPAY_F2FPAY_RESULT_SUCCESS);
         } elseif (tradeError($data)) {
--- tests/Refund/ClientTest.php
+++ tests/Refund/ClientTest.php
@@ -9,7 +9,9 @@
 namespace Kaylyu\Alipay\Tests\Refund;
 
 
+use Faker\Factory;
 use Kaylyu\Alipay\F2fpay\Application;
+use Kaylyu\Alipay\F2fpay\Base\Model\Builder\AlipayTradeRefundContentBuilder;
 use Kaylyu\Alipay\Tests\TestCase;
 
 class ClientTest extends TestCase
@@ -20,25 +22,67 @@ class ClientTest extends TestCase
     {
         $this->application = new Application(
             [
-                'response_type' => 'collection',
+                'response_type' => 'collection',//collection array json object
                 'log' => [
-                    'file' => __DIR__ . '/logs/lvmama.log',
+                    'file' => __DIR__ . '/logs/kaylu-alipay.log',
                     'level' => 'debug',
                 ],
                 'http' => [
-                    'base_uri' => '180.168.128.250:8081/scenic/order/',
                     'timeout' => 30,
                 ],
+                //当面付
+                'f2fpay' => [
+                    //签名方式,默认为RSA2(RSA2048)
+                    'sign_type' => "RSA2",
+
+                    //支付宝公钥
+                    'alipay_public_key' => "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAg02engaotvfFa9Nf680s8t4EUX8/qetOGLWrWNW7w4+zE+4uMaifxngkys2DbqSbj7nt/6lC9jArtHJSvn/6Eefh8A+MRtZRbzHlJ7WUl2wS4y6/mZrhP0YtzenHnAtgVpFUXgRQ8ZBZpgAxqNiqWQpjmNBPaQs7G2IRTsgsvH0kNi7PFgwWpsu2zZKfMHr4xHRcU0XsLbFLW59k5w/PsizVoRhBWAmQoltMteqXjAZgk55cRlxwDiAafWrqdJQTIE4GUJ0UiJhv/sTdDHpt+CVSEI2nJOOMrk7tsjhjQ97GKxXyb+Td4VS1HOHt8/jSOD/CSyjYG/v+j45XiA+6RQIDAQAB",
+
+                    //商户私钥
+                    'merchant_private_key' => "MIIEpQIBAAKCAQEAtDGh9DbiLTmLFCE2Cj5AW9xBAu7KOUx7HVy1CjAneM9m+6ZxYqR60yv4FmcJl1fu9k4Pth8BoXsTmjkQ152AVGMWxq9Q8COvjh0iYMZtkjGxURLDza5qyD4kHX+GoLFkoZjy1zJMfOW6gMaS6Xstd6t1i9lyMPz2QmelrMbbrBZM0iGrzcUCc0d7AnnXhxp3eppL0rMK++ZRawqmm1MzGSwDo/phtBPVtMSuHkJL+Tzq1k/FfWTthmIf5O003oaWbXmf/06z4aOSEPvdF/q4owrMPnjdPUU7TWllISSCb3ffjfeHI4WloJea5PURKo2tpPliV3CFdGTT13xvHNmzjwIDAQABAoIBAQCZB2GcDhQtBiZ39PB46H8txDKt8+9nk+0mdXoGafjLn4+8/ZBjjn4E4t1w4RaDIhl9sbOkHlb7NZPjML1973Nxtyk7mN3q4tOPpGxytXGOa4dD6+S/w3VKE4QGTSwfQifNGSB642uT0mF+RaW0hXiKneFY4FUbB2l34aAQyFz8mhLZGiQ2hDQpKvHX8Kn9MpqLdFo/iARSgK9kXJ+fhWKkHSVq4ZMi/17RbGyo6UhK/pbc4LGdUKcE8Pp5/8hdfQW6epHzECxrbBg4XwRcUaOONKuA6JlHHFVg2v5zM7dTs9aXZNBSeTlWgHuF8tLxxnSyMSP0Eqgzi8J/kKkXHOGpAoGBANhgtXRxPzdyeyZNvp7keUOEtko3lRDRGWKJMUmWOxRoBxIfAuJJLz2v2AuWJo6AaeLLkrlPw5hIHacCc0Rxyjnl25Iv06+uxW8sbc5j0VlstETMmiXMsNE3YMNoKw+8Bcq+/XA8r+uwIO4yBHdOznPG6JaM7I9y7zsBjjiiVD0VAoGBANUwtNVTvEgF7ktWTcT6m69EL38NJ17KT1+rODPd2bKtX6VMnfruv1QmAhd2EHvk6TFlEnNhI9FJgkeb98TONENKGKsLgAqiqctjSWCfAOHYo3RDBce1u2AYSSrV9KqoZFv8e8bKBA8Y+3cIkH4qgQ0S1BEdgzEKZ+BOsA4tjT8TAoGBAJKuRHPyTUN+NOOLcnxuL5JIxfZb5Y5n4Uh97k6PK++ycZCkOyVBLWgo61E3cV3KyXz4OberzdFOmeNcct1kMg27t3RLPbprxOWPfcSawBFZ8n5tINYdA4RggRUWaZKaCGcfv9i1GyCv3jL67zId1zB/F67vS3IXHPuoG0xjJq3dAoGAPzfkR+Y+zPTFRx3ejezqwbxsgTZ7WEaR5wK09dxxs+RpmnDv3/twTRnAQQjHtpWY+QbJw+EoZ+VudqGvhXLIWLPeQMp2O7EkvKVCmx5Leq1tOwqs6h3f5I0PFV7A4OJycDycz8QUFA4Kc6/ceS1Ne3z72d9widhWGVP8LRgg7HUCgYEA1ij0go6hXSG+rDsTJJC/tcN03HS7kzyrZ3IvPG1UgO9PydO1aOJXdm/porw4jC/NkrAd8XsbPjfNzOs70zLOEfDxGBdIhZSFKA/0mc7NSIOkuvXIB106vwYuzxAvlOfSleLv7buPgB11f26Zak5Y6042/2QLEQ0bv7xaz2pu2tc=",
+
+                    //编码格式
+                    'charset' => "UTF-8",
+
+                    //支付宝网关
+                    'gateway_url' => "https://openapi.alipaydev.com/gateway.do",
+
+                    //应用ID
+                    'app_id' => "2016101100663012",
+
+                    //异步通知地址,只有扫码支付预下单可用
+                    'notify_url' => "http://www.baidu.com",
+                ]
             ]
         );
     }
 
     /**
-     * 申请退款接口
+     * 统一收单交易退款接口
      * @author kaylv <kaylv@dayuw.com>
      */
-    public function OrderCancel(){
+    public function testRefund(){
+        $faker = Factory::create();
+        ////获取商户订单号
+        $outTradeNo = '0478795592836';
 
+        //第三方应用授权令牌,商户授权系统商开发模式下使用
+        $appAuthToken = "";//根据真实值填写
 
+        //创建退款请求builder,设置参数
+        $refundRequestBuilder = new AlipayTradeRefundContentBuilder();
+        $refundRequestBuilder->setOutTradeNo($outTradeNo);
+        $refundRequestBuilder->setRefundAmount(0.5);
+        $refundRequestBuilder->setOutRequestNo($faker->bankAccountNumber);
+
+        $refundRequestBuilder->setAppAuthToken($appAuthToken);
+
+        //请求
+        $response = $this->application->refund->refund($refundRequestBuilder);
+
+        var_dump($response);
+        var_dump($response->tradeStatus);//此值可以与AlipayF2FPayResult定义的常量进行对比，判断响应数据是否正常
+        var_dump($response->response->code);
+        var_dump($response->sign);
     }
 }
